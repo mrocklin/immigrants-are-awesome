db9e5541c22930cf206ebbb180030989f8bec159
Matthew Rocklin <mrocklin@gmail.com>
2017-01-29 12:41:15 -0500

    clean up conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,23 +20,20 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = []
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = []
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-# THEME='/home/mrocklin/workspace/pelican-themes'
-# JINJA_EXTENSIONS = ['jinja2.ext.i18n']
-# PLUGIN_PATHS = [THEME]
-# PLUGINS = ['i18n_subsites']
+THEME='theme'
 
+STATIC_PATHS = ['images']
 
+PLUGIN_PATHS = ['/home/mrocklin/workspace/pelican-plugins']
+PLUGINS = ['random_article']
+RANDOM = 'random.html'
